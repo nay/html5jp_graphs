ffe28638b412d0a5cd9735e5a2949f69fd3efbea
Yasuko Ohba <y.ohba@everyleaf.com>
2008-06-23 17:07:26 +0900

    Updated uninstall.rb

--- uninstall.rb
+++ uninstall.rb
@@ -1,7 +1,7 @@
 # Delete html5jp JavaScript codes from public/javascripts.
 files = ["circle.js", "excanvas.js", "excanvas-compressed.js", "line.js", "radar.js", "vbar.js"]
 
-Dir.chdir(File.join(RAILS_ROOT, 'public', 'javascripts', 'html5jp_graphs')) do
+Dir.chdir(File.join(RAILS_ROOT, 'public', 'javascripts')) do
   files.each do |f|
     File.delete(f) if File.exist?(f)
   end
