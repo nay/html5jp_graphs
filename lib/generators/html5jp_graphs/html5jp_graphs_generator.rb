93103d5b4c52946fa43ce4cd883e5b048bf5b58c
nay3 <y.ohba@everyleaf.com>
2012-07-21 17:20:50 +0900

    modify generator

--- lib/generators/html5jp_graphs/html5jp_graphs_generator.rb
+++ lib/generators/html5jp_graphs/html5jp_graphs_generator.rb
@@ -2,11 +2,11 @@ class Html5jpGraphsGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_js_files
-    dest_dir = File.join(RAILS_ROOT, 'public', 'javascripts')
-    FileUtils.mkdirs(dest_dir) unless File.exist?(dest_dir)
-
-    Dir.glob("template/**/*.js").each do |path|
-      FileUtils.cp(path, dest_dir) unless File.exist?(File.join(dest_dir, File.basename(path)))
-    end
+    copy_file "excanvas/excanvas-compressed.js", "public/javascripts/excanvas-compressed.js"
+    copy_file "excanvas/excanvas.js", "public/javascripts/excanvas.js"
+    copy_file "graph/circle.js", "public/javascripts/circle.js"
+    copy_file "graph/line.js", "public/javascripts/line.js"
+    copy_file "graph/radar.js", "public/javascripts/radar.js"
+    copy_file "graph/vbar.js", "public/javascripts/vbar.js"
   end
 end
